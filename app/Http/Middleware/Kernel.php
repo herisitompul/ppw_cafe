0882c58447b685ade3cb83732bd2dfac72388a85
Mickael Sitompul <135317357+herisitompul@users.noreply.github.com>
2024-12-04 13:42:50 +0700

    Delete app directory

--- app/Http/Middleware/Kernel.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Kernel {
-    protected $routeMiddleware = [
-        'remember_me' => \App\Http\Middleware\RememberMeMiddleware::class,
-        'admin' => \App\Http\Middleware\AdminMiddleware::class,
-    ];
-}
